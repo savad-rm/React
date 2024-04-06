b493da7404112188a7e5a158724cf646e2df1edd
hakeem <hakeem@codeap.com>
2024-04-06 17:53:01 +0530

    gallery images changes

--- src/compontent/galleryCard.jsx
+++ src/compontent/galleryCard.jsx
@@ -1,46 +1,30 @@
 
 import React, { useState, useEffect } from 'react';
 import '../style/gallery.css';
+import G1 from '../assets/img/home/banner2.jpg';
+import G2 from '../assets/img/home/2.jpeg';
+import G3 from '../assets/img/home/3.jpeg';
+import G4 from '../assets/img/home/banner1.jpg';
 
 const Gallery = () => {
   const [selectedImage, setSelectedImage] = useState(null);
-  const [isPlaying] = useState(false);
-  const [setCurrentIndex] = useState(0);
 
   const data = [
     {
-      imageUrl: 'https://www.jdtislamnewhope.com/img/event/3.jpg',
-      description: 'JDT ISLAM COLLEGES GRADUATION CEREMONY-2023, Ms. TIFFANY BRAR',
+      image:G1,
+      description: 'JDT ISLAM SPECIAL SCHOOL ACTIVITIES',
     },
     {
-      imageUrl: 'https://www.jdtislamnewhope.com/img/event/1.jpg',
-      description: 'JDT ISLAM COLLEGES GRADUATION CEREMONY-2023, Ms. TIFFANY BRAR',
+      image:G2,
+      description: 'JDT ISLAM SPECIAL SCHOOL ACTIVITIES',
+    },  {
+      image:G3,
+      description: 'JDT ISLAM SPECIAL SCHOOL ACTIVITIES',
     },
     {
-      imageUrl: 'https://www.jdtislamnewhope.com/img/event/4.jpg',
-      description: 'JDT ISLAM COLLEGES GRADUATION CEREMONY-2023, Ms. TIFFANY BRAR',
-    },
-    {
-      imageUrl: 'https://www.jdtislamnewhope.com/img/event/2.jpg',
-      description: 'Description for Image 4',
-    },
-    {
-      imageUrl: 'https://www.jdtislamnewhope.com/img/event6.jpeg',
-      description: 'JDT ISLAM COLLEGES GRADUATION CEREMONY-2023, Ms. TIFFANY BRAR',
-    },
-    {
-      imageUrl: 'https://www.jdtislamnewhope.com/img/event5.jpeg',
-      description: 'JDT ISLAM COLLEGES GRADUATION CEREMONY-2023, Ms. TIFFANY BRAR',
-    }, {
-      imageUrl: 'https://www.jdtislamnewhope.com/img/event3.jpeg',
-      description: 'JDT ISLAM COLLEGES GRADUATION CEREMONY-2023, Ms. TIFFANY BRAR',
-    }, {
-      imageUrl: 'https://www.jdtislamnewhope.com/img/event1.jpeg',
-      description: 'JDT ISLAM COLLEGES GRADUATION CEREMONY-2023, Ms. TIFFANY BRAR',
-    }, {
-      imageUrl: 'https://www.jdtislamnewhope.com/img/event2.jpeg',
-      description: 'JDT ISLAM COLLEGES GRADUATION CEREMONY-2023, Ms. TIFFANY BRAR',
-    },
+      image:G4,
+      description: 'JDT ISLAM SPECIAL SCHOOL ACTIVITIES',
+    }
   ];
 
   const openLightbox = (index) => {
@@ -52,20 +36,6 @@ const Gallery = () => {
     setSelectedImage(null);
     document.body.style.overflow = 'scroll'; 
   };
-  useEffect(() => {
-    let intervalId;
-
-    if (isPlaying) {
-      intervalId = setInterval(() => {
-        setCurrentIndex((prevIndex) => (prevIndex + 1) % data.length);
-      }, 2000); 
-    }
-
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, [isPlaying, data.length]);
-
   return (
     <div className="gallery-main">
       <section className='gallery-card'>
@@ -74,7 +44,7 @@ const Gallery = () => {
         {data.map((item, index) => (
           <div key={index} className="col-md-4">
             <div className="image-container" onClick={() => openLightbox(index)}>
-              <img src={item.imageUrl} alt={item.name} className="gallery-image" />
+              <img src={item.image} alt={item.name} className="gallery-image" />
               <div className="image-info">
                 <p>{item.description}</p>
               </div>
@@ -87,7 +57,7 @@ const Gallery = () => {
       {selectedImage !== null && (
         <div className="lightbox">
           <span className="close-button" onClick={closeLightbox}>×</span>
-          <img src={data[selectedImage].imageUrl} alt={{selectedImage}} className="lightbox-image" />
+          <img src={data[selectedImage].image} alt={{selectedImage}} className="lightbox-image" />
           <div className="lightbox-info">
             {/* Add lightbox information here if needed */}
           </div>
